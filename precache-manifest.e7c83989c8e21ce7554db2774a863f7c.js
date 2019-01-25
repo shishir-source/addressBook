f2fe6c17f43fb1b11e8f6a797f8b501e9154ec1d
Your Name <shishir323@gmail.com>
2019-01-26 05:55:08 +0600

    Updates

---
+++ precache-manifest.e7c83989c8e21ce7554db2774a863f7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b344cc905191c705e5d8",
+    "url": "/addressBook/static/js/runtime~main.b344cc90.js"
+  },
+  {
+    "revision": "966bd7957aa1ec29c964",
+    "url": "/addressBook/static/js/main.966bd795.chunk.js"
+  },
+  {
+    "revision": "1107e9f65428fb1fe326",
+    "url": "/addressBook/static/js/1.1107e9f6.chunk.js"
+  },
+  {
+    "revision": "966bd7957aa1ec29c964",
+    "url": "/addressBook/static/css/main.3b2bc9ed.chunk.css"
+  },
+  {
+    "revision": "1107e9f65428fb1fe326",
+    "url": "/addressBook/static/css/1.2956c5e5.chunk.css"
+  },
+  {
+    "revision": "e33a792887fa832d70de1eb71a9849f7",
+    "url": "/addressBook/index.html"
+  }
+];
